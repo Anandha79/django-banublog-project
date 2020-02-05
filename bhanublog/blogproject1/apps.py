58131453bee434534886f20da586bd1c67152134
Bhanu <anandhachinna@gmail.com>
2020-02-05 13:05:28 +0530

    firstcommit

---
+++ bhanublog/blogproject1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Blogproject1Config(AppConfig):
+    name = 'blogproject1'
---
+++ bhanublog/blogproject1/templatetags/blogproject1_tags.py
@@ -0,0 +1,17 @@
+from blogproject1.models import Post
+from django import template
+register=template.Library()
+
+@register.simple_tag(name='my_tag')
+def total_posts():
+    return Post.objects.count()
+
+@register.inclusion_tag('blogproject1/latest_post123.html')
+def show_latest_posts(count=4):
+    latest_posts=Post.objects.order_by('-published')[:count]
+    return {'latest_posts':latest_posts}
+
+from django.db.models import Count
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.objects.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
